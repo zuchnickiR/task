bf5033afbb745547dd4d3271b5e1c2b36964b508
mateuszostrowski <mateusz.ostrowski.lbn@gmail.com>
2021-09-28 19:42:28 +0200

    create project

---
+++ SmartWarehouse/src/main/java/pl/infoshare/App.java
@@ -0,0 +1,13 @@
+package pl.infoshare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Yellow Angels" );
+    }
+}
